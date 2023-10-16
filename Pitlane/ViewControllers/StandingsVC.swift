6b5ae6114cb7de335f435bbe3ca1de1857eec714
Adam Zapiór <adamzapior@icloud.com>
2023-10-17 01:33:17 +0200

    feat: TableViewHeader with scrollViewDidScroll behaviour

--- Pitlane/ViewControllers/StandingsVC.swift
+++ Pitlane/ViewControllers/StandingsVC.swift
@@ -4,122 +4,97 @@ import UIKit
 
 class StandingsVC: UIViewController {
     var vm = StandingsVM()
-    
-    private let scrollView: UIScrollView = {
-        let sv = UIScrollView()
-        return sv
+
+    private let activityIndicator: UIActivityIndicatorView = {
+        let ai = UIActivityIndicatorView()
+        ai.style = .large
+        ai.color = UIColor.red
+        return ai
     }()
 
-    private let containerView: UIView = {
-        let view = UIView()
+    private let tableViewHeader: TableViewHeader = {
+        let view = TableViewHeader()
+        view.backgroundColor = .systemBackground
         return view
     }()
-    
-    private let activityIndicator = UIActivityIndicatorView()
-    //    = {
-    //        let indicator = UIActivityIndicatorView(style: .large)
-    //        indicator.color = .red
-    ////        indicator.translatesAutoresizingMaskIntoConstraints = false
-    //        return indicator
-    //    }
-    //    ()
-    
-    private let segmentedControl: UISegmentedControl = {
-        let standingType = ["Drivers", "Constructors"]
-        let sc = UISegmentedControl(items: standingType)
-        return sc
-    }()
-    
+
     private let tableView: UITableView = {
         let tableView = UITableView()
         tableView.backgroundColor = .systemBackground
         tableView.allowsSelection = true
         tableView.register(StandingsCell.self, forCellReuseIdentifier: StandingsCell.identifier)
         tableView.isHidden = true
-        
         return tableView
     }()
-    
-    // Lifecycle
+
+    // MARK: Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         vm.delegate = self
-        
         tableView.delegate = self
         tableView.dataSource = self
-        
-//        setupActivityIndicator()
-        activityIndicator.startAnimating()
-        
+
         setupUI()
+        activityIndicator.startAnimating()
+
+        scrollViewDidScroll(tableView)
 
         Task {
             await vm.fetchStandings()
         }
-        
+    }
+
+    // MARK: UI Setup
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView == tableView {
+            if tableView.contentOffset.y > 10.0 {
+                navigationController?.navigationBar.prefersLargeTitles = false
+            }
+
+            if tableView.contentOffset.y <= 10.0 {
+                navigationController?.navigationBar.prefersLargeTitles = true
+                navigationItem.largeTitleDisplayMode = .automatic
+                navigationController?.navigationBar.sizeToFit()
+            }
+        }
+    }
+
+    private func setupUI() {
         title = "Standings"
         view.backgroundColor = .systemBackground
-        navigationController?.navigationBar.prefersLargeTitles = true
-    }
-    
-    // MARK: UI Setup
-    
-    private func setupUI() {
-        view.addSubview(scrollView)
-        scrollView.addSubview(containerView)
-        
-        setupStandingsTypeControl()
-        containerView.addSubview(tableView)
-        containerView.addSubview(activityIndicator)
-        
-        scrollView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-        }
 
-        containerView.snp.makeConstraints { make in
-            make.top.bottom.left.right.equalTo(scrollView)
-            make.width.equalTo(scrollView)
-        }
-        
-        
-        
-        view.addSubview(tableView)
-        tableView.snp.makeConstraints { make in
-            make.top.equalTo(segmentedControl.snp.bottom).offset(10)
-            make.left.right.bottom.equalToSuperview()
-        }
-        
-        setupActivityIndicator()
-    }
-    
-    private func setupActivityIndicator() {
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.largeTitleDisplayMode = .automatic
+        navigationController?.navigationBar.sizeToFit()
+        tableView.contentInsetAdjustmentBehavior = .never
+
         view.addSubview(activityIndicator)
         activityIndicator.snp.makeConstraints { make in
             make.center.equalToSuperview()
         }
-        activityIndicator.style = .large
-        activityIndicator.color = UIColor.red
-    }
-    
-    private func setupStandingsTypeControl() {
-//        let standingType = ["Drivers", "Constructors"]
-//        let segmentedControl = UISegmentedControl(items: standingType)
-        segmentedControl.addTarget(self, action: #selector(standingTypeDidChange(_:)), for: .valueChanged)
-        
-        view.addSubview(segmentedControl)
-        segmentedControl.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide).offset(10) // zakładając, że chcesz go blisko górnej krawędzi, ale z małym odstępem
-            make.left.equalToSuperview().offset(10) // dodaj odstęp od lewej krawędzi
-            make.right.equalToSuperview().offset(-10) // dodaj odstęp od prawej krawędzi
+
+        view.addSubview(tableView)
+        tableView.tableHeaderView = tableViewHeader
+
+        tableViewHeader.standingTypeChanged = { [weak self] segmentedControl in
+            self?.standingTypeDidChange(segmentedControl)
+        }
+
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.left.right.bottom.equalToSuperview()
         }
     }
-    
+
+    // MARK: Selectors
+
     @objc private func standingTypeDidChange(_ segmentedControl: UISegmentedControl) {
         switch segmentedControl.selectedSegmentIndex {
         case 0: break
         case 1: break
-        default: 0
+        default: break
         }
     }
 }
@@ -128,7 +103,7 @@ extension StandingsVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
         return vm.driverStandings.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "StandingsCell", for: indexPath) as? StandingsCell else {
             fatalError("Custom cell error")
@@ -141,17 +116,11 @@ extension StandingsVC: UITableViewDelegate, UITableViewDataSource {
 extension StandingsVC: StandingsVMDelegate {
     func standingsDidUpdate(_: StandingsVM, standings _: [DriverStandingModel]) {
         print("Data loaded")
-        
+
         DispatchQueue.main.async {
             self.tableView.reloadData()
-            
             self.activityIndicator.stopAnimating()
             self.tableView.isHidden = false
-        }
-//        tableView.reloadData()
-
-        for standing in vm.standings {
-            print(standing.season)
         }
 
         for driverStanding in vm.driverStandings {
@@ -164,8 +133,6 @@ extension StandingsVC: StandingsVMDelegate {
     }
 
     func driverStandingsDidUpdate(_: StandingsVM, driverStandings _: [DriverStandingModel]) {
-//        for driverStanding in viewModel.driverStandings {
-//            print(driverStanding.driver.familyName)
-//        }
+        //
     }
 }
--- Pitlane/Views/F1BodyLabel.swift
+++
@@ -1,44 +0,0 @@
-//
-//  F1BodyLabel.swift
-//  F1 Stats
-//
-//  Created by Adam Zapiór on 18/12/2022.
-//
-
-import UIKit
-
-class F1BodyLabel: UILabel {
-
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        configure() //
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    init(textAlignnment: NSTextAlignment) { //Setup UI Label for being customizable
-        super.init(frame: .zero)
-        self.textAlignment = textAlignnment
-        configure() //
-    }
-    
-    
-    private func configure(){
-        textColor                 = .label
-        font                      = UIFont.preferredFont(forTextStyle: .body) //
-        adjustsFontSizeToFitWidth = true
-        minimumScaleFactor        = 0.9
-        lineBreakMode             = .byWordWrapping
-        translatesAutoresizingMaskIntoConstraints = false
-    }
-}
-
-
-
-#Preview {
-    F1BodyLabel()
-}
-
-
